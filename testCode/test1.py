91b337e7c4b8d8d010c42c8c06f2739bf5480277
kevinGames32 <kevinplaythinks@gmail.com>
2026-02-05 11:54:26 -0500

    added work for daily challenge 3379, easy, tranformed array

--- testCode/test1.py
+++ testCode/test1.py
@@ -1,12 +1,8 @@
-from collections import Counter
-from typing import List
+def print_word(s:str):
+    with open("logs.txt", "w") as f:
+        for i in range(len(s)):
+            for j in range(0, i+1):
+                print(s[j], file=f, end="")
+            print("", file=f)
 
-class Solution:
-    def frequencySort(self, nums: List[int]) -> List[int]:
-        count = sorted(Counter(nums).items(), key=lambda x: (x[1], -x[0]))
-        ans = []
-
-        for tup in count:
-            for i in range(tup[1]):
-                ans.append(tup[0])
-        return ans
+print_word(s="kevin")
